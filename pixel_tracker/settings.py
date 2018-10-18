b7852d0d605d92ee0cd1d1d4484f4f179b004f42
Sheldon Smickley <sheldon.smickley@gmail.com>
2018-10-18 13:30:21 -0400

    updated gitignore

--- pixel_tracker/settings.py
+++ pixel_tracker/settings.py
@@ -42,9 +42,11 @@ INSTALLED_APPS = [
     'pixel',
     'pixel_tracker',
     'zappa_django_utils',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'pixel_tracker.urls'
@@ -74,6 +77,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'pixel_tracker.wsgi.application'
 
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_METHODS = ('GET')
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
